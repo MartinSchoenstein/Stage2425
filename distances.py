abb0d2870b95d7767531063709a1c91646b2dc3d
Pauline <pauline.mermillod@outlook.fr>
2024-12-10 10:49:27 +0100

    add Fisher test wip

--- distances.py
+++ distances.py
@@ -2,48 +2,78 @@ import pandas as pd
 import numpy as np
 from sklearn.metrics import jaccard_score
 from scipy.spatial import distance
-from scipy import linalg, stats
+from scipy import stats
 from sklearn.metrics import mutual_info_score
 from sklearn import preprocessing
 from newick import read
 from Bio import Phylo
+from sqlite3 import Binary
+from scipy import stats
+from scipy.stats import fisher_exact
 
 
-def distance_profiles(x, y, method, parameter = False):
+def distance_profiles(
+    method,
+    x,
+    y=None,
+    successive_transitions=True,
+    confidence=1.5,
+    penalty=0.6,
+    truncation=None,
+):
     if isinstance(x, str):
-        dfx, dfy = input(x, y)
+        dfx = input(x)
     else:
-        dfx, dfy = x, y
-    if method == "Jaccard":
+        dfx = x
+    if isinstance(y, str):
+        dfy = input(y)
+    else:
+        dfy = y
+    if method == "Jaccard" or method == "jaccard":
         return jaccard(dfx, dfy)
-    if method == "Hamming":
+    if method == "Hamming" or method == "hamming":
         return hamming(dfx, dfy)
-    if method == "Pearson":
+    if method == "Pearson" or method == "pearson":
         return pearson(dfx, dfy)
-    if method == "MI":
+    if method == "MI" or method == "mi":
         return mi(dfx, dfy)
-    if method == "cotransition":
-        return cotransition(dfx, dfy, parameter)
-    if method == "SVDphy":
-        return SVD_phy(A, p)
+    if method == "cotransition" or method == "Cotransition":
+        return (dfx, dfy, successive_transitions)
+    if method == "pcs" or method == "PCS":
+        return pcs(dfx, dfy, confidence, penalty)
+    if method == "svd_phy" or method == "SVD_phy":
+        return svd_phy(dfx, truncation)
 
 
-def input(x, y):
+def input(x):
     dfx = pd.read_csv(x, sep="\t", index_col=0)
-    dfy = pd.read_csv(y, sep="\t", index_col=0)
     global binary
     binary = is_binary(dfx)
-    return (dfx, dfy)
+    return dfx
 
 
 def jaccard(dfx, dfy):
     if binary == False:
         return "Binary profiles only ; use to_Binary() fonction"
     else:
-        jaccard_distance = pd.DataFrame(index=list(dfx.index), columns=list(dfy.index))
-        for i in dfx.index:
-            for j in dfy.index:
-                jaccard_distance.loc[i, j] = 1 - jaccard_score(dfx.loc[i], dfy.loc[j])
+        if dfy is not None:
+            jaccard_distance = pd.DataFrame(
+                index=list(dfx.index), columns=list(dfy.index)
+            )
+            for i in dfx.index:
+                for j in dfy.index:
+                    jaccard_distance.loc[i, j] = 1 - jaccard_score(
+                        dfx.loc[i], dfy.loc[j]
+                    )
+        else:
+            jaccard_distance = pd.DataFrame(
+                index=list(dfx.index), columns=list(dfx.index)
+            )
+            for i in dfx.index:
+                for j in dfx.index:
+                    jaccard_distance.loc[i, j] = 1 - jaccard_score(
+                        dfx.loc[i], dfx.loc[j]
+                    )
     print("Jaccard Distance :")
     return jaccard_distance
 
@@ -52,10 +82,20 @@ def hamming(dfx, dfy):
     if binary == False:
         return "Binary profiles only ; use to_Binary() fonction"
     else:
-        hamming_distance = pd.DataFrame(index=dfx.index, columns=dfy.index)
-        for i in dfx.index:
-            for j in dfy.index:
-                hamming_distance.loc[i, j] = distance.hamming(dfx.loc[i], dfy.loc[j])
+        if dfy is not None:
+            hamming_distance = pd.DataFrame(index=dfx.index, columns=dfy.index)
+            for i in dfx.index:
+                for j in dfy.index:
+                    hamming_distance.loc[i, j] = distance.hamming(
+                        dfx.loc[i], dfy.loc[j]
+                    )
+        else:
+            hamming_distance = pd.DataFrame(index=dfx.index, columns=dfx.index)
+            for i in dfx.index:
+                for j in dfx.index:
+                    hamming_distance.loc[i, j] = distance.hamming(
+                        dfx.loc[i], dfx.loc[j]
+                    )
     print("Hamming Distance :")
     return hamming_distance
 
@@ -65,11 +105,24 @@ def pearson(dfx, dfy):
         print(
             "You use continuous profiles, think about normalize your datas with normalize() function"
         )
-    pearson_results = pd.DataFrame(index=dfx.index, columns=dfy.index)
-    for i in dfx.index:
-        for j in dfy.index:
-            pearson_correlation = stats.pearsonr(dfx.loc[i], dfy.loc[j])
-            pearson_results.loc[i, j] = [pearson_correlation[0], pearson_correlation[1]]
+    if dfy is not None:
+        pearson_results = pd.DataFrame(index=dfx.index, columns=dfy.index)
+        for i in dfx.index:
+            for j in dfy.index:
+                pearson_correlation = stats.pearsonr(dfx.loc[i], dfy.loc[j])
+                pearson_results.loc[i, j] = [
+                    pearson_correlation[0],
+                    pearson_correlation[1],
+                ]
+    else:
+        pearson_results = pd.DataFrame(index=dfx.index, columns=dfx.index)
+        for i in dfx.index:
+            for j in dfx.index:
+                pearson_correlation = stats.pearsonr(dfx.loc[i], dfx.loc[j])
+                pearson_results.loc[i, j] = [
+                    pearson_correlation[0],
+                    pearson_correlation[1],
+                ]
     print("Pearson Correlation and associated p-values :")
     return pearson_results
 
@@ -79,69 +132,216 @@ def mi(dfx, dfy):
         print(
             "You use continuous profiles, think about normalize your datas with normalize() function"
         )
-    mi_distance = pd.DataFrame(index=dfx.index, columns=dfy.index)
-    for i in dfx.index:
-        for j in dfy.index:
-            mi_distance.loc[i, j] = mutual_info_score(dfx.loc[i], dfy.loc[j])
+    if dfy is not None:
+        mi_distance = pd.DataFrame(index=dfx.index, columns=dfy.index)
+        for i in dfx.index:
+            for j in dfy.index:
+                mi_distance.loc[i, j] = mutual_info_score(dfx.loc[i], dfy.loc[j])
+    else:
+        mi_distance = pd.DataFrame(index=dfx.index, columns=dfx.index)
+        for i in dfx.index:
+            for j in dfx.index:
+                mi_distance.loc[i, j] = mutual_info_score(dfx.loc[i], dfx.loc[j])
     print("Mutual Information :")
     return mi_distance
 
 
-def cotransition(tvx, tvy, parameter = False):
+def cotransition(tvx, tvy, successive_transitions=True):
     print("Take care to use transition vectors and not classic profiles")
     if binary == False:
         return "Binary profiles only ; use to_Binary() fonction"
-    cotransition_scores = pd.DataFrame(index=tvx.index, columns=tvy.index)
-    for i in tvx.index:
-        for j in tvy.index:
-            t1 = 0
-            t2 = 0
-            c = 0
-            d = 0
-            k = 0
-            last_transition_x = ""
-            last_transition_y = ""
-            for x in range(0, len(tvx.columns)):
-                if parameter == False or last_transition_x != x -1:
-                    if tvx.loc[i][x] != 0:
-                        t1 = t1 + 1
-                if parameter == False or last_transition_y != x -1:
-                    if tvy.loc[j][x] !=0:
-                        t2 = t2 + 1
-                if (last_transition_x != x - 1 and last_transition_y != x -1) or parameter == False:
-                    if tvx.loc[i][x] != 0 and tvy.loc[j][x] != 0:
-                        if tvx.loc[i][x] == tvy.loc[j][x]:
-                            c = c + 1
-                        else:
-                            d = d + 1
-            k = c -d
-            cotransition_scores.loc[i, j] = k / (t1 + t2 - abs(k))
+    if tvy is not None:
+        cotransition_scores = pd.DataFrame(index=tvx.index, columns=tvy.index)
+        for i in tvx.index:
+            for j in tvy.index:
+                t1 = 0
+                t2 = 0
+                c = 0
+                d = 0
+                k = 0
+                last_transition_x = ""
+                last_transition_y = ""
+                for x in range(0, len(tvx.columns)):
+                    if successive_transitions == True or last_transition_x != x - 1:
+                        if tvx.loc[i][x] != 0:
+                            t1 = t1 + 1
+                    if successive_transitions == True or last_transition_y != x - 1:
+                        if tvy.loc[j][x] != 0:
+                            t2 = t2 + 1
+                    if (
+                        last_transition_x != x - 1 and last_transition_y != x - 1
+                    ) or successive_transitions == True:
+                        if tvx.loc[i][x] != 0 and tvy.loc[j][x] != 0:
+                            if tvx.loc[i][x] == tvy.loc[j][x]:
+                                c = c + 1
+                            else:
+                                d = d + 1
+                k = c - d
+                if t1 == 0 and t2 == 0:
+                    cotransition_scores.loc[i, j] = None
+                else:
+                    cotransition_scores.loc[i, j] = k / (t1 + t2 - abs(k))
+                #tableau de contingence:
+                contingency_table = [[abs(k),t1-abs(k)], [t2-abs(k),(x+1)-t1-t2+abs(k)]]
+                score = fisher_exact(contingency_table, alternative="greater")
+                p_values.loc[i, j] = score.pvalue
+    else:
+        cotransition_scores = pd.DataFrame(index=tvx.index, columns=tvx.index)
+        for i in tvx.index:
+            for j in tvx.index:
+                t1 = 0
+                t2 = 0
+                c = 0
+                d = 0
+                k = 0
+                last_transition_x = ""
+                last_transition_y = ""
+                for x in range(0, len(tvx.columns)):
+                    if successive_transitions == True or last_transition_x != x - 1:
+                        if tvx.loc[i][x] != 0:
+                            t1 = t1 + 1
+                    if successive_transitions == True or last_transition_y != x - 1:
+                        if tvx.loc[j][x] != 0:
+                            t2 = t2 + 1
+                    if (
+                        last_transition_x != x - 1 and last_transition_y != x - 1
+                    ) or successive_transitions == True:
+                        if tvx.loc[i][x] != 0 and tvx.loc[j][x] != 0:
+                            if tvx.loc[i][x] == tvx.loc[j][x]:
+                                c = c + 1
+                            else:
+                                d = d + 1
+                k = c - d
+                print(i, j, x, t1, t2, c, d, k)
+                if t1 == 0 and t2 == 0:
+                    cotransition_scores.loc[i, j] = None
+                else:
+                    cotransition_scores.loc[i, j] = k / (t1 + t2 - abs(k))
+                    #tableau de contingence:
+                contingency_table = [[abs(k),t1-abs(k)], [t2-abs(k),(x+1)-t1-t2+abs(k)]]
+                score = fisher_exact(contingency_table, alternative="greater")
+                p_values.loc[i, j] = score.pvalue
     print("Cotransition score :")
-    return cotransition_scores
+    return cotransition_scores, p_values
 
 
-def SVD_phy(a, p):
+def pcs(tvx, tvy, confidence=1.5, penalty=0.6):
+    print("Take care to use transition vectors and not classic profiles")
+    if binary == False:
+        return "Binary profiles only ; use to_Binary() fonction"
+    if tvy is not None:
+        pcs_scores = pd.DataFrame(index=tvx.index, columns=tvy.index)
+        for i in tvx.index:
+            for j in tvy.index:
+                match_1 = 0
+                mismatch_1 = 0
+                match_2 = 0
+                mismatch_2 = 0
+                for x in range(1, len(tvx.columns)):
+                    if tvx.loc[i][x] != 0 or tvy.loc[j][x] != 0:
+                        if tvx.loc[i][x] == tvy.loc[j][x]:
+                            if len(tvx.columns) - 1 - x > 0:
+                                if (
+                                    tvx.loc[i][x - 1]
+                                    == tvx.loc[i][x + 1]
+                                    == tvy.loc[j][x - 1]
+                                    == tvy.loc[j][x + 1]
+                                    == 0
+                                ):
+                                    match_2 = match_2 + 1
+                                else:
+                                    match_1 = match_1 + 1
+                            else:
+                                match_1 = match_1 + 1
+                        elif len(tvx.columns) - 1 - x > 0:
+                            if (
+                                tvx.loc[i][x] != 0
+                                and tvx.loc[i][x - 1] == tvx.loc[i][x + 1] == 0
+                            ) or (
+                                tvy.loc[j][x] != 0
+                                and tvy.loc[j][x - 1] == tvy.loc[j][x + 1] == 0
+                            ):
+                                mismatch_2 = mismatch_2 + 1
+                            else:
+                                mismatch_1 = mismatch_1 + 1
+                        else:
+                            mismatch_1 = mismatch_1 + 1
+                pcs_scores.loc[i, j] = (
+                    (match_1)
+                    + (match_2 * confidence)
+                    - penalty * (mismatch_1 + mismatch_2 * confidence)
+                )
+    else:
+        pcs_scores = pd.DataFrame(index=tvx.index, columns=tvx.index)
+        for i in tvx.index:
+            for j in tvx.index:
+                match_1 = 0
+                mismatch_1 = 0
+                match_2 = 0
+                mismatch_2 = 0
+                for x in range(1, len(tvx.columns)):
+                    if tvx.loc[i][x] != 0 or tvx.loc[j][x] != 0:
+                        if tvx.loc[i][x] == tvx.loc[j][x]:
+                            if len(tvx.columns) - 1 - x > 0:
+                                if (
+                                    tvx.loc[i][x - 1]
+                                    == tvx.loc[i][x + 1]
+                                    == tvx.loc[j][x - 1]
+                                    == tvx.loc[j][x + 1]
+                                    == 0
+                                ):
+                                    match_2 = match_2 + 1
+                                else:
+                                    match_1 = match_1 + 1
+                            else:
+                                match_1 = match_1 + 1
+                        elif len(tvx.columns) - 1 - x > 0:
+                            if (
+                                tvx.loc[i][x] != 0
+                                and tvx.loc[i][x - 1] == tvx.loc[i][x + 1] == 0
+                            ) or (
+                                tvx.loc[j][x] != 0
+                                and tvx.loc[j][x - 1] == tvx.loc[j][x + 1] == 0
+                            ):
+                                mismatch_2 = mismatch_2 + 1
+                            else:
+                                mismatch_1 = mismatch_1 + 1
+                        else:
+                            mismatch_1 = mismatch_1 + 1
+                pcs_scores.loc[i, j] = (
+                    (match_1)
+                    + (match_2 * confidence)
+                    - penalty * (mismatch_1 + mismatch_2 * confidence)
+                )
+    print("PCS :")
+    return pcs_scores
 
-    U, S, V = np.linalg.svd(,False) #SVD de la matrice A
-    k = int(p * len(U)) #nb de colonnes à garder dans U
-    U_truncated = U[:, :k] #ajout des colonnes U
+
+def svd_phy(a, p=0.5):
+    u, s, v = np.linalg.svd(a, False)  # SVD de la matrice A
+    k = int(p * len(u))  # nb de colonnes à garder dans U
+    u_truncated = u[:, :k]  # ajout des colonnes U
 
     # normalisation
-    for i in range(0, len(U_truncated)):
+    for i in range(0, len(u_truncated)):
         for j in range(0, k):
-            U_truncated[i][j] = U_truncated[i][j] / np.max(U_truncated[i])
+            u_truncated[i][j] = u_truncated[i][j] / np.max(u_truncated[i])
 
     # Calculate the distance euclidienne
-    SVDphy_distance = np.zeros((len(U_truncated), len(U_truncated)))
-    for i in range(0, len(U_truncated)):
-        for j in range(0, len(U_truncated)):
-            SVDphy_distance[i][j] = np.sqrt(np.sum((U_truncated[i] - U_truncated[j])**2))
-    
+    svdphy_distance = np.zeros((len(u_truncated), len(u_truncated)))
+    for i in range(0, len(u_truncated)):
+        for j in range(0, len(u_truncated)):
+            svdphy_distance[i][j] = np.sqrt(
+                np.sum((u_truncated[i] - u_truncated[j]) ** 2)
+            )
+
     row_labels = a.index
     col_labels = a.index
-    SVDphy_distance = pd.DataFrame(SVDphy_distance, index=row_labels, columns=col_labels)
+    svdphy_distance = pd.DataFrame(
+        svdphy_distance, index=row_labels, columns=col_labels
+    )
 
-    return SVDphy_distance
+    return svdphy_distance
 
 
 def to_binary(path, treshold=60):
@@ -157,14 +357,7 @@ def to_binary(path, treshold=60):
                     df.iloc[i][j] = 0
         return df
     else:
-        return "Already binary profiles"
-
-
-# def normalize(df):
-#   for i in range(0, len(df)):
-#      for j in range(0, len(df.columns)):
-#         df.iloc[i][j] = df.iloc[i][j] / max(df.iloc[i])
-# return df
+        return print("Already binary profiles")
 
 
 def normalize(path):
@@ -201,6 +394,7 @@ def transition_vector(path):
 
 def is_binary(df):
     for x in df.iloc[0]:
+
         if x not in [0, 1]:
             global binary
             binary = False
@@ -221,4 +415,3 @@ def order_by_tree(x, tree):
     for l in leaf:
         ordered_df[str(l)] = dfx[str(l)]
     return ordered_df
-
